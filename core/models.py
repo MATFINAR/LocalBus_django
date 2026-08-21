8b6d53cf14eff304b767e3cf17dde098cadb510b
MATFINAR <mtabordaf2102@gmail.com>
2026-08-20 19:42:11 -0500

    se creo la clase ruta en el archivo modelo del core

--- core/models.py
+++ core/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+class Ruta(models.Model):
+    id_ruta = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    origen = models.CharField(max_length=200)
+    destino = models.CharField(max_length=200)
+    distancia_Km = models.FloatField(10,2)
+    duracion_estimada = models.TimeField()
+    estado = models.CharField(50)
+    fecha_creacion = models.DateTimeField()
+    frecuencia = models.TimeField()
+    paradas = models.IntegerField(max_length=100)
