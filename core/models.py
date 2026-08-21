58532b02e1a5e3930d37326955386546a81d6be1
MATFINAR <mtabordaf2102@gmail.com>
2026-08-20 20:45:54 -0500

    se creo la clase bus y conductor en el archivo modelo del core

--- core/models.py
+++ core/models.py
@@ -1,5 +1,30 @@
 from django.db import models
 
+class Ruta(models.Model):
+    id_ruta = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    origen = models.CharField(max_length=200)
+    destino = models.CharField(max_length=200)
+    distancia_Km = models.FloatField(10,2)
+    duracion_estimada = models.TimeField()
+    estado = models.CharField(50)
+    fecha_creacion = models.DateTimeField()
+    frecuencia = models.TimeField()
+    paradas = models.IntegerField(max_length=100)
+    
+class Conductor(models.Model):
+    id_conductor = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100)
+    cedula = models.CharField(max_length=50)
+    contrasena =models.CharField(max_length=40) 
+
+    id_ruta = models.ForeignKey( 'Ruta',on_delete=models.CASCADE, related_name='conductores')
+
+    def __str__(self):
+        return self.nombre
+
 class Bus(models.Model):
     id_bus = models.AutoField(primary_key=True)
     placa = models.CharField(max_length=20, unique=True)
