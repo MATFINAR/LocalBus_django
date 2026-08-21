18f237c29a8bf9e1bc7fc018fb88f0935d002b76
alejo vasco <vascovelezalejo@gmail.com>
2026-08-20 19:42:24 -0500

    se creo la clase Bus en el archivo models del core

--- core/models.py
+++ core/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+from django.db import models
+
+class Bus(models.Model):
+    id_bus = models.AutoField(primary_key=True)
+    placa = models.CharField(max_length=20, unique=True)
+    id_ruta = models.IntegerField()
