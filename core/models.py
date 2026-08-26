3995ffed2ddf2c2df5e07695d0e9edd55cd09319
MATFINAR <mtabordaf2102@gmail.com>
2026-08-26 17:43:55 -0500

    Se implemento la base de datos con PostgreSQL

--- core/models.py
+++ core/models.py
@@ -5,12 +5,15 @@ class Ruta(models.Model):
     nombre = models.CharField(max_length=100)
     origen = models.CharField(max_length=200)
     destino = models.CharField(max_length=200)
-    distancia_Km = models.FloatField(10,2)
+    distancia_Km = models.FloatField()
     duracion_estimada = models.TimeField()
-    estado = models.CharField(50)
+    estado = models.CharField(max_length=50)
     fecha_creacion = models.DateTimeField()
     frecuencia = models.TimeField()
-    paradas = models.IntegerField(max_length=100)
+    paradas = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
     
 class Conductor(models.Model):
     id_conductor = models.AutoField(primary_key=True)
