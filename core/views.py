a5b3ee836b89666669965fb06e58c61d7e7e32ab
alejo vasco <vascovelezalejo@gmail.com>
2026-08-27 19:48:22 -0500

    funciona listar conductores

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+from django.http import request
 from django.shortcuts import render
+from .models import Conductor
 
 def home(request):
     return render(request, 'core/home.html')
@@ -11,7 +13,13 @@ def alertas(request):
 def rutas(request):
     return render(request, 'core/rutas.html')
 def conductores(request):
-    return render(request, 'core/conductores.html')
+    conductores = Conductor.objects.all()
+
+    return render(
+        request,
+        'core/conductores.html',
+        {'conductores': conductores}
+    )
 def buses(request):
     return render(request, 'core/buses.html')
 def acerca_de(request):
