8bcadbba2218c012adf88c2f1c34d9254df16b8c
Juancho <higuitalj@gmail.com>
2026-08-27 19:48:06 -0500

    Funciona Listar Buses

--- core/models.py
+++ core/models.py
@@ -5,27 +5,72 @@ class Ruta(models.Model):
     nombre = models.CharField(max_length=100)
     origen = models.CharField(max_length=200)
     destino = models.CharField(max_length=200)
-    distancia_Km = models.FloatField(10,2)
-    duracion_estimada = models.TimeField()
-    estado = models.CharField(50)
-    fecha_creacion = models.DateTimeField()
-    frecuencia = models.TimeField()
-    paradas = models.IntegerField(max_length=100)
-    
-class Conductor(models.Model):
-    id_conductor = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=15)
-    email = models.EmailField(max_length=100)
-    cedula = models.CharField(max_length=50)
-    contrasena =models.CharField(max_length=40) 
-
-    id_ruta = models.ForeignKey( 'Ruta',on_delete=models.CASCADE, related_name='conductores')
+    distancia_Km = models.FloatField(default=0)
+    duracion_estimada = models.TimeField(default='00:00:00')
+    estado = models.CharField(max_length=50)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    frecuencia = models.TimeField(default='00:00:00')
+    paradas = models.IntegerField(default=0)
 
     def __str__(self):
         return self.nombre
+ 
+class Conductor(models.Model):
+    id_conductor = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    cedula = models.CharField(max_length=50, unique=True)
+    contrasena = models.CharField(max_length=40) 
+
+    ruta = models.ForeignKey(
+        Ruta,
+        on_delete=models.CASCADE,
+        related_name='conductores'
+    )
+
+    def __str__(self):
+        return f"{self.nombre} - {self.cedula}"
 
 class Bus(models.Model):
     id_bus = models.AutoField(primary_key=True)
     placa = models.CharField(max_length=20, unique=True)
-    id_ruta = models.IntegerField()
+    ruta = models.ForeignKey(
+        Ruta,
+        on_delete=models.CASCADE,
+        related_name='buses'
+    )
+
+    def __str__(self):
+        return self.placa
+
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=100, unique=True)
+    contrasena = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    nickName = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.nickName
+
+class Alerta(models.Model):
+    id_alerta = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE, 
+        related_name='alertas'
+    )
+    ruta = models.ForeignKey(
+        Ruta,
+        on_delete=models.CASCADE, 
+        related_name="alertas"
+    )
+    tipo = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+    estado = models.CharField(max_length=50)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.descripcion
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Bus
 
 def home(request):
     return render(request, 'core/home.html')
@@ -12,7 +13,11 @@ def rutas(request):
     return render(request, 'core/rutas.html')
 def conductores(request):
     return render(request, 'core/conductores.html')
+
 def buses(request):
-    return render(request, 'core/buses.html')
+    #para listar los buses en la vista
+    buses = Bus.objects.all()
+    return render(request, 'core/buses.html', {'buses': buses})
+
 def acerca_de(request):
     return render(request, 'core/acerca_de.html')
