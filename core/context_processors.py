b046bd7cb3abdadd488571bf97dcb395480120ab
Miguel Taborda <mtabordaf2102.com>
2026-09-17 11:28:39 -0500

    error del mostrar usuario registrado en todos los templates, ademas de correccion de sangria en todos los html

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,11 +1,19 @@
 from .models import Usuario
 
 def usuario_actual(request):
-    """Hace disponible el usuario logueado en TODOS los templates"""
+    """
+    Hace disponible el usuario logueado en todos los templates.
+    """
+
+    usuario_perfil = None
     usuario_id = request.session.get('usuario_id')
+
     if usuario_id:
         try:
-            return {'usuario_perfil': Usuario.objects.get(id_usuario=usuario_id)}
+            usuario_perfil = Usuario.objects.get(id_usuario=usuario_id)
         except Usuario.DoesNotExist:
-            return {'usuario_perfil': None}
-    return {'usuario_perfil': None}
+            usuario_perfil = None
+
+    return {
+        'usuario_perfil': usuario_perfil,
+    }
